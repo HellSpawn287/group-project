b8f7ca72c2cbd19423c8e7759523589090b19f5b
Karol <karol44k@gmail.com>
2018-02-20 20:17:52 +0100

    Add Event

---
+++ src/main/java/com/github/java5wro/event/Event.java
@@ -0,0 +1,80 @@
+package com.github.java5wro.event;
+
+import org.h2.engine.User;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String uuid = UUID.randomUUID().toString();
+    private String name;
+    private LocalDate time;
+    private String description;
+    private Integer price;
+    private User author;
+
+    public Event() {
+    }
+
+    public Event(String name, LocalDate time, String description, Integer price) {
+        this.name = name;
+        this.time = time;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDate time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
