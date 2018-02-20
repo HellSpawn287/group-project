e7cf89787fc4482bebcb1c82451fb87470b3096c
Robert Prus <robert.prus87@gmail.com>
2018-02-20 19:10:11 +0100

    Add new branch 'ticketBranch'. Add new class 'Ticket'

--- src/main/java/com/github/java5wro/ticket/Ticket.java
+++ src/main/java/com/github/java5wro/ticket/Ticket.java
@@ -1,12 +1,4 @@
-//package com.github.java5wro.ticket;
-//
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//public class Ticket {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long id;
-//}
+package com.github.java5wro.ticket;
+
+public class Ticket {
+}
