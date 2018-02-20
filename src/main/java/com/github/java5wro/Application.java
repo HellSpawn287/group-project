31bde500825e057a0d566495e1e8b3286db9d9a9
xenteros <adam@gurgul.com.pl>
2018-02-20 18:33:11 +0100

    Add project structure (#1)
    * Add maven directory structure,
    * Add packages for each model class,
    * Add parent and spring-web to pom.xml
    * Add psvm
    * Add application.properties

---
+++ src/main/java/com/github/java5wro/Application.java
@@ -0,0 +1,12 @@
+package com.github.java5wro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
