9b119f6e8f239990cb37a67a15d0499a1d5692f0
Tomasz Tomczyk <tomasz.m.tomczyk@gmail.com>
2018-02-20 20:21:58 +0100

    Add Swagger configuration

--- src/main/java/com/github/java5wro/Application.java
+++ src/main/java/com/github/java5wro/Application.java
@@ -2,11 +2,44 @@ package com.github.java5wro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("java5wro")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/api.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Ticket Service Application")
+                .description("Spring REST Ticket Service Application")
+                .contact("java5wro")
+                .license("Apache License Version 2.0")
+                .version("2.0")
+                .build();
+    }
+
+
+
 }
