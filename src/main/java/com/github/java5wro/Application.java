99a1031ced5d53a8d836f03594159a684c9c5d30
Magdalena Domanska <m.z.domanska@gmail.com>
2018-02-20 19:02:24 +0100

    Merge branch 'develop' of /media/magda/518BF9B75CAB5A45/KursJava/GroupProject/group-project with conflicts.

--- src/main/java/com/github/java5wro/Application.java
+++ src/main/java/com/github/java5wro/Application.java
@@ -2,11 +2,18 @@ package com.github.java5wro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan("com.github.java5wro")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+
 }
