9fd61a7e139cfa7725b23f79c09dc38cb6834197
Patryk Szustak <patryk.szustak6@gmail.com>
2018-02-22 18:49:34 +0100

    Add all Security
    * added security package
    * added SecurityConfiguration Class
    * added MyUserDetailsService
    * added Security dependency to Maven
    * added new methods to UserService
    * added interface to User Class

---
+++ src/main/java/com/github/java5wro/user/security/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.github.java5wro.user.security;
+
+import com.github.java5wro.user.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin@admin.pl").password("admin1").roles("ROLE_ADMIN")
+                .and()
+                .withUser("user@user.pl").password("user1").roles("ROLE_USER");
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider
+                = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(myUserDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/start").permitAll()
+                .antMatchers("/admin").hasRole("ROLE_ADMIN")
+                .antMatchers("/user").hasRole("ROLE_USER")
+                .anyRequest().authenticated();
+        http.httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+}
