a4589c5efe151de8c016f0aeab4b1c83b24edf58
Tomasz Tomczyk <tomasz.m.tomczyk@gmail.com>
2018-02-22 18:54:14 +0100

    Add @Email
    Added @Email in User

--- src/main/java/com/github/java5wro/user/model/User.java
+++ src/main/java/com/github/java5wro/user/model/User.java
@@ -1,5 +1,6 @@
 package com.github.java5wro.user.model;
 
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,6 +25,7 @@ public class User implements UserDetails{
     @NotBlank
     private String name;
     @NotBlank
+    @Email
     private String email;
     @NotBlank
     private String password;
