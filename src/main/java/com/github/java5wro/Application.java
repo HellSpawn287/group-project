8bf7dd7da9fc3c8a57e8960899fb0e44e9e859ad
Olga Kromuszczynska <okromuszczynska@gmail.com>
2018-02-22 18:38:56 +0100

    Add simple mail sending

--- src/main/java/com/github/java5wro/Application.java
+++ src/main/java/com/github/java5wro/Application.java
@@ -1,12 +1,27 @@
 package com.github.java5wro;
 
+import com.github.java5wro.email.EmailService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(Application.class, args);
+
+
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner (EmailService es) {
+        return args -> {
+            es.sendEmail("okromuszczynska@gmail.com", "test2", "DUPA!!!");
+        };
     }
 }
