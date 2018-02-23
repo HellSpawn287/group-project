e5e4333189d3cb44f1a0fb917618c16e2f0c7c00
Zuzanna Korycka <zuzanna.korycka@gmail.com>
2018-02-23 19:01:15 +0100

    Add parameter to method emailSender

--- src/main/java/com/github/java5wro/email/EmailService.java
+++ src/main/java/com/github/java5wro/email/EmailService.java
@@ -27,21 +27,21 @@ public class EmailService implements EmailSender {
     }
 
     @Override
-    public void sendEmail(String to, String subject, String content) throws IOException {
+    public void sendEmail(String name, String eventName, String uuid, String price, String date, String userEmail) throws IOException {
         MimeMessage mail = javaMailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-            helper.setTo(to);
+            helper.setTo(userEmail);
             helper.setReplyTo("eventmail5wro@gmail.com");
             helper.setFrom("eventmail5wro@gmail.com");
-            helper.setSubject(subject);
-            helper.setText(content, false);
+            helper.setSubject("Your ticket to " + eventName);
+            helper.setText("Thank you for buying ticket for our event.", false);
 
             PdfGenerator pdf = new PdfGenerator();
-            ByteArrayDataSource attachment = new ByteArrayDataSource(pdf.createPdf(),"aplication/pdf");
+            ByteArrayDataSource attachment = new ByteArrayDataSource(pdf.createPdf(name, eventName, uuid),"aplication/pdf");
             helper.addAttachment("Ticket.pdf",attachment);
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-            ByteArrayDataSource attachment1 = new ByteArrayDataSource(invoiceGenerator.invoiceGenerator(),"aplication/xls");
+            ByteArrayDataSource attachment1 = new ByteArrayDataSource(invoiceGenerator.invoiceGenerator(name, price, date),"aplication/xls");
             helper.addAttachment("Invoice.xls",attachment1);
 
 
