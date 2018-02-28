d7298d6fe13df352ecb14d177975a32c8a347841
Tomasz Ziemiański <free100@op.pl>
2018-02-28 20:03:26 +0100

    Minor improves of html pages
    * outlook and messages improved

--- src/main/java/com/github/java5wro/user/VerificationToken.java
+++ src/main/java/com/github/java5wro/user/VerificationToken.java
@@ -1,72 +1,72 @@
-package com.github.java5wro.user;
-
-import org.apache.tomcat.jni.User;
-
-import javax.persistence.*;
-import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Date;
-
-@Entity
-public class VerificationToken {
-    private static final int EXPIRATION = 60 * 24;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String token;
-
-    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "user_id")
-    private User user;
-
-    private Date expiryDate;
-
-    private Date calculateExpiryDate(int expiryTimeInMinutes) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Timestamp(cal.getTime().getTime()));
-        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
-        return new Date(cal.getTime().getTime());
-    }
-
-    public VerificationToken(){
-
-    }
-
-    public static int getEXPIRATION() {
-        return EXPIRATION;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Date getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(Date expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-}
+//package com.github.java5wro.user;
+//
+//import org.apache.tomcat.jni.User;
+//
+//import javax.persistence.*;
+//import java.sql.Timestamp;
+//import java.util.Calendar;
+//import java.util.Date;
+//
+//@Entity
+//public class VerificationToken {
+//    private static final int EXPIRATION = 60 * 24;
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    private String token;
+//
+//    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+//    @JoinColumn(nullable = false, name = "user_id")
+//    private User user;
+//
+//    private Date expiryDate;
+//
+//    private Date calculateExpiryDate(int expiryTimeInMinutes) {
+//        Calendar cal = Calendar.getInstance();
+//        cal.setTime(new Timestamp(cal.getTime().getTime()));
+//        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+//        return new Date(cal.getTime().getTime());
+//    }
+//
+//    public VerificationToken(){
+//
+//    }
+//
+//    public static int getEXPIRATION() {
+//        return EXPIRATION;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getToken() {
+//        return token;
+//    }
+//
+//    public void setToken(String token) {
+//        this.token = token;
+//    }
+//
+//    public User getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(User user) {
+//        this.user = user;
+//    }
+//
+//    public Date getExpiryDate() {
+//        return expiryDate;
+//    }
+//
+//    public void setExpiryDate(Date expiryDate) {
+//        this.expiryDate = expiryDate;
+//    }
+//}
