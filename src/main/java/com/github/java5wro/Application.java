702996c4971e26a1046882d8edb1c2e0a4da87b3
Patryk Szustak <patryk.szustak6@gmail.com>
2018-02-28 19:43:56 +0100

    Add registration-verify
    added EmailMessage Class
    added VerificationToken Class
    added VerificationController Class

--- src/main/java/com/github/java5wro/Application.java
+++ src/main/java/com/github/java5wro/Application.java
@@ -7,12 +7,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Properties;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -57,5 +61,7 @@ public class Application {
                 .version("2.0")
                 .build();
     }
+
+
 
 }
