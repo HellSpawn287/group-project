efc45391c12f5d135ecddb15bffadb03033ccec0
Krzysztof Wojtas <32910091+Wojtas5600@users.noreply.github.com>
2018-03-01 18:55:52 +0100

    Add email, ticket and invoice generators (#13)

--- src/main/java/com/github/java5wro/email/EmailService.java
+++ src/main/java/com/github/java5wro/email/EmailService.java
@@ -1,20 +1,14 @@
 package com.github.java5wro.email;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.InputStreamSource;
+import com.github.java5wro.ticket.TicketForEmail;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import javax.mail.util.ByteArrayDataSource;
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Service
 public class EmailService implements EmailSender {
@@ -27,6 +21,69 @@ public class EmailService implements EmailSender {
     }
 
     @Override
+    public void sendEmail(String name, String eventName, String uuid, String price, String date, String userEmail) throws IOException {
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            helper.setTo(userEmail);
+            helper.setReplyTo("eventmail5wro@gmail.com");
+            helper.setFrom("eventmail5wro@gmail.com");
+            helper.setSubject("Your ticket to " + eventName);
+            helper.setText("Thank you for buying ticket for our event.", false);
+
+            PdfGenerator pdf = new PdfGenerator();
+            ByteArrayDataSource attachment = new ByteArrayDataSource(pdf.createPdf(name, eventName, uuid), "aplication/pdf");
+            helper.addAttachment("Ticket.pdf", attachment);
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            ByteArrayDataSource attachment1 = new ByteArrayDataSource(invoiceGenerator.invoiceGenerator(name, price, date), "aplication/xls");
+            helper.addAttachment("Invoice.xls", attachment1);
+
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        javaMailSender.send(mail);
+    }
+
+    @Override
+    public void sendEmail(TicketForEmail ticketForEmail) throws IOException {
+
+        if (ticketForEmail == null) {
+            throw new IllegalArgumentException("Ticket for email mustnt be null");
+        }
+        sendEmail(ticketForEmail.getUsername(),
+                ticketForEmail.getEventName(),
+                ticketForEmail.getUuid(),
+                ticketForEmail.getPrice(),
+                ticketForEmail.getPurchaseDate().toString(),
+                ticketForEmail.getEmail());
+    }
+
+    public MimeMessageHelper basicMessageHelperSetup(String to, String subject, String content) throws MessagingException {
+
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+        helper.setTo(to);
+        helper.setReplyTo("eventmail5wro@gmail.com");
+        helper.setFrom("eventmail5wro@gmail.com");
+        helper.setSubject(subject);
+        helper.setText(content, false);
+
+        return helper;
+    }
+
+    @Override
+    public void sendEmailWithoutTicket(String to, String subject, String content) {
+
+        try {
+            javaMailSender.send(basicMessageHelperSetup(to, subject, content).getMimeMessage());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
     public void sendEmail(String to, String subject, String content) throws IOException {
         MimeMessage mail = javaMailSender.createMimeMessage();
         try {
@@ -36,15 +93,6 @@ public class EmailService implements EmailSender {
             helper.setFrom("eventmail5wro@gmail.com");
             helper.setSubject(subject);
             helper.setText(content, false);
-
-            PdfGenerator pdf = new PdfGenerator();
-            ByteArrayDataSource attachment = new ByteArrayDataSource(pdf.createPdf(),"aplication/pdf");
-            helper.addAttachment("Ticket.pdf",attachment);
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-            ByteArrayDataSource attachment1 = new ByteArrayDataSource(invoiceGenerator.invoiceGenerator(),"aplication/xls");
-            helper.addAttachment("Invoice.xls",attachment1);
-
-
 
         } catch (MessagingException e) {
             e.printStackTrace();
