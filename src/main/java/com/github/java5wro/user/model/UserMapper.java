2a2591f2808526d8bfecda30ed90d3586642a63f
xenteros <adam@gurgul.com.pl>
2018-03-05 18:56:00 +0100

    WIP: Mapstruct example

--- src/main/java/com/github/java5wro/user/model/UserMapper.java
+++ src/main/java/com/github/java5wro/user/model/UserMapper.java
@@ -1,9 +1,14 @@
 package com.github.java5wro.user.model;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mappings({
+        @Mapping(source = "email", target = "mail")
+    })
     UserDTO toUserDTO(UserEntity userEntity);
 }
