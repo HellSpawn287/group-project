ed8e9fb7ece85788b96ba9345475b21024bb70f7
Robert Prus <robert.prus87@gmail.com>
2018-03-05 20:06:44 +0100

    Reformat TicketMapper

--- src/main/java/com/github/java5wro/ticket/TicketMapper.java
+++ src/main/java/com/github/java5wro/ticket/TicketMapper.java
@@ -1,13 +1,20 @@
 package com.github.java5wro.ticket;
 
+import org.mapstruct.Mapper;
+
+import org.mapstruct.ReportingPolicy;
+
+import java.util.Set;
+
 /**
  * Created by Lenovo on 2018-02-28.
  */
-public class TicketMapper {
-    public static TicketEntity toTicketEntity(TicketDTO ticketDTO){
-        return new TicketEntity(ticketDTO.getId(),ticketDTO.getUuid(),ticketDTO.getEvent(),ticketDTO.getPurchaseDate(),ticketDTO.getOwner());
-    }
-    public static TicketDTO toTicketDTO(TicketEntity entity){
-        return new TicketDTO(entity.getId(), entity.getUuid(),entity.getEvent(),entity.getPurchaseDate(),entity.getOwner());
-    }
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface TicketMapper {
+
+    TicketEntity toTicketEntity(TicketDTO ticketDTO);
+    Set<TicketEntity> setToTicketEntity(TicketDTO ticketDTO);
+
+    TicketDTO toTicketDTO(TicketEntity entity);
+    Set<TicketDTO> setToTicketDTO(TicketEntity entity);
 }
