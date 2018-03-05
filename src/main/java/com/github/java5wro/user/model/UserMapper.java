d78d7b9eec0d0dc8a0832e64cc88cc3ea63f5031
xenteros <adam@gurgul.com.pl>
2018-03-05 18:59:49 +0100

    WIP: Mapstruct example

--- src/main/java/com/github/java5wro/user/model/UserMapper.java
+++ src/main/java/com/github/java5wro/user/model/UserMapper.java
@@ -3,15 +3,17 @@ package com.github.java5wro.user.model;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.Collection;
 import java.util.Set;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface UserMapper {
 
     @Mappings({
-        @Mapping(source = "email", target = "mail")
+            @Mapping(source = "email", target = "mail"),
+            @Mapping(target = "confirmPassword", ignore = true)
     })
     UserDTO toUserDTO(UserEntity userEntity);
 
